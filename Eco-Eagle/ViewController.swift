26e5c3210ade7d1a1356f1855670f0006531bb1a
jessxkang <kang782455@sas.edu.sg>
2022-06-17 14:42:17 +0900

    buttons in room

--- Eco-Eagle/ViewController.swift
+++ Eco-Eagle/ViewController.swift
@@ -14,10 +14,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        let calculatorURL = URL(string: "https://carbon-calculator.climatehero.me/?source=GoogleKeywords&gclid=CjwKCAjwqauVBhBGEiwAXOepkYQZ91WoW-f-6ytNanqB7TBaCZBM9Q0h7SkWVKt-v6ge8FGuwev9YhoCbzQQAvD_BwE")
-        myWK.load(URLRequest(url: calculatorURL!))
-    }
+        
+       
+        }
 
-
+   
+    
 }
 
